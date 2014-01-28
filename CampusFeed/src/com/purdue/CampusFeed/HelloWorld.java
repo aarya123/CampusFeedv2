3669abdbdb4df58f431df1bf9c7f8f50446a024b
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-01-27 23:23:22 -0500

    Simple activity and layout

---
+++ CampusFeed/src/com/purdue/CampusFeed/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.purdue.CampusFeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: AnubhawArya
+ * Date: 1/27/14
+ * Time: 11:21 PM
+ */
+public class HelloWorld extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld);
+    }
+}
