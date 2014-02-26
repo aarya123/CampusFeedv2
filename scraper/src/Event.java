46256145523c0f80e5d934aaccd97873de16c244
Dan Green <green131@purdue.edu>
2014-02-26 18:07:21 -0500

    Added Event Class

---
+++ scraper/src/Event.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Event {
+
+	private String url;
+	private String title;
+	private String location;
+	private Date time_start;
+	private Date time_end;
+	
+	public Event(String url) {
+		
+	}
+
+}
