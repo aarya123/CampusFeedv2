ff3736008600ffdf65eb4a783171c014209dcd52
Dan Green <green131@purdue.edu>
2014-02-26 19:30:51 -0500

    Completed scraper (part 1)
    Scraper.java
    - Switched to XML feed for my own sanity
    - Scraper now scans the updated XML page for events
    - Each event has a Event object that holds the data for that event
    - Scraper now parses and retrieves event data
    
    Event.java
    - Object to hold event data for each event
    - Created Get/Set methods for useability purposes
    - Object is created with URL

--- scraper/src/Event.java
+++ scraper/src/Event.java
@@ -4,12 +4,85 @@ public class Event {
 
 	private String url;
 	private String title;
+	private String author;
+	private String category;
 	private String location;
-	private Date time_start;
-	private Date time_end;
+	private String time_start;
+	private String time_end;
+	private String description;
 	
 	public Event(String url) {
-		
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public String getLocation() {
+		return location;
 	}
 
+	public void setStartTime(String startTime) {
+		this.time_start = startTime;
+	}
+	
+	public String getStartTime() {
+		return time_start;
+	}
+	
+	public void setEndTime(String endTime) {
+		this.time_end = endTime;
+	}
+	
+	public String getEndTime() {
+		return time_end;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String toString() {
+		return url + "\n"
+				+ title + "\n"
+				+ author + "\n"
+				+ category + "\n"
+				+ location + "\n"
+				+ time_start + "\n"
+				+ time_end + "\n"
+				+ description;
+	}
 }
--- scraper/src/scraper.java
+++ scraper/src/scraper.java
@@ -6,40 +6,65 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-public class scraper {
+public class Scraper {
 		
 	private static void runScanner(String website) {
 		String webURL = "the internet.";
 		try {
 			if (website.equals("BoilerLink")) {
-				webURL = "https://boilerlink.purdue.edu/Events";
+				webURL = "https://boilerlink.purdue.edu/EventRss/EventsRss";
 				scanBoilerLink(webURL);
 			}
 		} catch (IOException e) {
-			System.out.println("ERROR: Unable to connect to "+website+" at "+webURL);
+			System.out.println("ERROR: Unable to connect to "+website+" at "+webURL+
+					"\nTry again in a few seconds.");
 			e.printStackTrace();
 		}
 	}
 	
 	private static void scanBoilerLink(String url) throws IOException {
 		Connection com = Jsoup.connect(url);
-		Document scan_homepage = com.get();
-		Elements links = scan_homepage.select("a[href]");
-		for (Element link : links) {
-			if (link.hasClass("modal") && link.hasAttr("target")) {
-				String page_url = link.absUrl("href");
-				//TODO DEBUG
-				System.out.println("Grabbing data from: " + page_url);
-				getBoilerPageData(page_url);
-			}
+		Document scan_homepage = com.ignoreContentType(true).get();
+		Elements events = scan_homepage.select("item");
+		for (Element event : events) {
+			parseBoilerEvent(event);
+			//TODO DEBUG TESTING
+			//System.out.println("-");
 		}
 	}
 	
-	private static void getBoilerPageData(String url) throws IOException {
-		Connection com_page = Jsoup.connect(url);
-		Document scan_page = com_page.get();
-		Elements links = scan_page.select("h2");
-		System.out.println(links.get(0).select("span").text());
+	public static void parseBoilerEvent(Element event) {
+		String page_url = event.text().split("<link />")[0].split(" ")[0];
+		Event k = new Event(page_url);
+		k.setAuthor(event.select("author").text());
+		k.setCategory(event.select("category").text());
+		k.setTitle(event.select("title").text());
+		
+		String description = event.select("description").text();
+		String[] times = description.split("<span class=\"");
+		
+		String startTime = times[1];
+		if (startTime.equals("dtstart\">")) {
+			k.setStartTime("");
+		} else {
+			startTime = startTime.split("title=\"")[1].split("\">")[0];
+			k.setStartTime(startTime);
+		}
+		k.setEndTime(times[2].split("</span>")[0].split("title=\"")[1].split("\">")[0]);
+		k.setLocation(description.split("<span class=\"location\">")[1].split("</span>")[0]);
+		
+		description = description.split("<div class=\"description\">")[1];
+		if (description.equals("</div>")) {
+			k.setDescription("");
+		} else {
+			description = description.split("</div>")[0];
+			description = description.replace("<p>", "");
+			description = description.replace("</p>", "");
+			description = description.replace("&nbsp;", "");
+			k.setDescription(description);
+		}
+		//TODO DEBUG TESTING
+		//System.out.println(k.toString());
 	}
 	
 	public static void main(String[] args) {
