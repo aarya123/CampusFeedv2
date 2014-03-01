e198f0625a97005905a8988245b3badc184fd786
mgottein <mgottein@purdue.edu>
2014-03-01 16:13:01 -0500

    basic config

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -8,7 +8,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("hello world");
+    }
+    
+    public static Result create_user() {
+    	return TODO;
     }
 
 }
--- server/build.sbt
+++ server/build.sbt
@@ -2,10 +2,14 @@ name := "server"
 
 version := "1.0-SNAPSHOT"
 
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.26"
+
+
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
   cache
-)     
+)
+
 
 play.Project.playJavaSettings
