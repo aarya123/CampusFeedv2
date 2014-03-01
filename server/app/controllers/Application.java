56515cc490c5d901d1217aa483afe8d2e8020dfa
mgottein <mgottein@purdue.edu>
2014-03-01 16:55:57 -0500

    basic database writing works

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -1,8 +1,15 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.*;
+import play.db.DB;
+import play.mvc.*;
+import play.mvc.Http.RequestBody;
 import views.html.*;
 
 public class Application extends Controller {
@@ -11,8 +18,26 @@ public class Application extends Controller {
         return ok("hello world");
     }
     
+    private static final String CREATE_USER_SQL = "INSERT INTO users (name) VALUES (?)";
     public static Result create_user() {
-    	return TODO;
+    	RequestBody request = request().body();
+    	JsonNode obj = request.asJson();
+    	String name = null;
+    	if(obj != null) {
+    		name = obj.get("name").textValue();
+    	}
+    	if(name == null) {
+    		return badRequest("usage: json object with name");
+    	}
+    	try(Connection connection = DB.getConnection()) {
+    		PreparedStatement stmt = connection.prepareStatement(CREATE_USER_SQL);
+    		stmt.setString(1, name);
+    		stmt.execute();
+    	}
+    	catch(SQLException e) {
+    		e.printStackTrace();
+    	}
+    	return ok("ok");
     }
 
 }
