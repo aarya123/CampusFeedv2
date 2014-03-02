3d0d53dc119a123f35cf71334e1db16ffa0efd84
mgottein <mgottein@purdue.edu>
2014-03-02 00:15:19 -0500

    added first name and last name to login. updated naming

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -48,29 +48,33 @@ public class Application extends Controller {
     	return query;
     }
     
-    private static final String LOGIN_USER_SQL = "INSERT INTO user (fb_user_id, access_token, expires) VALUES (?, ?, FROM_UNIXTIME(?)) ON DUPLICATE KEY UPDATE access_token=?, expires=FROM_UNIXTIME(?)";
+    private static final String LOGIN_USER_SQL = "INSERT INTO user (fb_user_id, first_name, last_name, access_token, expires) VALUES (?, ?, ?, ?, FROM_UNIXTIME(?)) ON DUPLICATE KEY UPDATE access_token=?, expires=FROM_UNIXTIME(?)";
     public static Result login() {
     	RequestBody request = request().body();
     	//retrieve access token param
     	JsonNode obj = request.asJson();
-    	String access_token = null;
+    	String accessToken = null;
     	if(obj != null) {
-    		access_token = obj.get("access_token").textValue();
+    		accessToken = obj.get("access_token").textValue();
     	}
-    	if(access_token == null) {
+    	if(accessToken == null) {
     		return badRequest("usage: json object with access_token");
     	}
-    	//retrieve user id
+    	//retrieve user id, first name, last name
     	long userId;
+    	String firstName;
+    	String lastName;
     	try {
     		JsonNode userInfo = WS.url("https://graph.facebook.com/me")
-    				.setQueryParameter("fields", "id")
-        			.setQueryParameter("access_token", access_token)
+    				.setQueryParameter("fields", "id,first_name,last_name")
+        			.setQueryParameter("access_token", accessToken)
         			.get()
         			.get(FB_TIMEOUT)
         			.asJson();
-    		if(userInfo.has("id")) {
+    		if(userInfo.has("id") && userInfo.has("first_name") && userInfo.has("last_name")) {
     			userId = userInfo.get("id").asLong();
+    			firstName = userInfo.get("first_name").textValue();
+    			lastName = userInfo.get("last_name").textValue();
     		}
     		else {
     			return badRequest(JsonNodeFactory.instance.objectNode().put("error", "invalid access token"));
@@ -85,7 +89,7 @@ public class Application extends Controller {
 		    	.setQueryParameter("grant_type", "fb_exchange_token")
 		    	.setQueryParameter("client_id", APP_ID)
 		    	.setQueryParameter("client_secret", APP_SECRET)
-		    	.setQueryParameter("fb_exchange_token", access_token)
+		    	.setQueryParameter("fb_exchange_token", accessToken)
 		    	.get()
 		    	.get(FB_TIMEOUT);
     	long expires = 0;
@@ -96,7 +100,7 @@ public class Application extends Controller {
     			return internalServerError();
     		}
     		else {
-    			access_token = respVals.get("access_token");
+    			accessToken = respVals.get("access_token");
     			expires = Long.parseLong(respVals.get("expires"));
     		}
     	}
@@ -108,11 +112,13 @@ public class Application extends Controller {
     	try(Connection connection = DB.getConnection()) {
     		PreparedStatement stmt = connection.prepareStatement(LOGIN_USER_SQL);
     		stmt.setLong(1, userId);
-    		stmt.setString(2, access_token);
+    		stmt.setString(2, firstName);
+    		stmt.setString(3, lastName);
+    		stmt.setString(4, accessToken);
     		long expiresTime = expires + (System.currentTimeMillis() / 1000L);
-    		stmt.setLong(3, expiresTime);
-    		stmt.setString(4,  access_token);
     		stmt.setLong(5, expiresTime);
+    		stmt.setString(6,  accessToken);
+    		stmt.setLong(7, expiresTime);
     		stmt.execute();
     	}
     	catch(SQLException e) {
@@ -120,7 +126,7 @@ public class Application extends Controller {
     		return internalServerError();
     	}
     	//return long lived access token to use as auth
-    	JsonNode resp = JsonNodeFactory.instance.objectNode().put("access_token", access_token);
+    	JsonNode resp = JsonNodeFactory.instance.objectNode().put("access_token", accessToken);
     	return Application.ok(resp);
     }
 
