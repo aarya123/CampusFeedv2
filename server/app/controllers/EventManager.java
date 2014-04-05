1962690564163b898ca8f7fafb4f57fc13b14a43
mgottein <mgottein@purdue.edu>
2014-04-04 22:36:33 -0400

    top5 fix

--- server/app/controllers/EventManager.java
+++ server/app/controllers/EventManager.java
@@ -737,28 +737,21 @@ public static Result top5() {
 	JsonNode request = request().body().asJson();
 	String category = request.get("category").textValue();
 	try(Connection conn = DB.getConnection()) {
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM `Event` WHERE category = ? ORDER BY view_count DESC LIMIT 5");
+		PreparedStatement stmt = conn.prepareStatement("select distinct Event.id as id, Event.name as name, Event.location as location, UNIX_TIMESTAMP(Event.time) as time, Event.description as description, Event.status as status from Event inner join Event_has_Tags on Event.id = Event_has_Tags.Event_id inner join Tags on Event_has_Tags.Tags_id = Tags.id where Tags.tag = ? ORDER BY Event.view_count DESC LIMIT 5");
 		stmt.setString(1, category);
-		ResultSet s = stmt.executeQuery();
-		ArrayNode res = JsonNodeFactory.instance.arrayNode();
-		JSONArray list = new JSONArray();
-		while(s.next()) {
-			JSONObject event = new JSONObject();
-			try {
-				event.put("title", s.getString("name"));
-				event.put("id", s.getString("id"));
-				event.put("desc", s.getString("description"));
-				event.put("date_time",s.getTimestamp("time"));
-				event.put("location", s.getString("location"));
-				event.put("view_count",s.getInt("view_count"));
-				event.put("category", s.getString("category"));
+		ResultSet rs = stmt.executeQuery();
+		ArrayNode searchResults = JsonNodeFactory.instance.arrayNode();
+		while(rs.next()) {
+			ObjectNode eventRes = createEventJson(rs);
+			try(PreparedStatement stmtTag = conn.prepareStatement("select Tags.tag from Tags INNER JOIN Event_has_Tags ON Tags.id = Event_has_Tags.Tags_id INNER JOIN Event ON Event.id = Event_has_Tags.Event_id WHERE Event.id = ?")) {
+				stmtTag.setLong(1, rs.getLong("id"));
+				stmtTag.execute();
+				ResultSet rsTag = stmtTag.getResultSet();
+				addCategoriesToEventJson(eventRes, rsTag);
 			}
-			catch(JSONException e) {
-				
-			}
-			list.put(event);
+			searchResults.add(eventRes);
 		}
-		return ok(list.toString());
+		return ok(searchResults);
 		
 	}
 	catch(SQLException e) {
