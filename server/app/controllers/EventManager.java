3fba86cf5b60c99f1172687e5d659e9466d6a3ba
mgottein <mgottein@purdue.edu>
2014-04-24 14:43:35 -0400

    err 2 check

--- server/app/controllers/EventManager.java
+++ server/app/controllers/EventManager.java
@@ -567,7 +567,7 @@ public static Result updateEvent()
 	}
 	catch(SQLException e) {
 		e.printStackTrace();
-		return ok("err", e.getStackTrace().toString());
+		return ok("err", e.getMessage());
 	}
 	
 }
@@ -660,10 +660,10 @@ public static Result getEventAttendees()
 	JsonNode request  = request().body().asJson();
 	// get all members for event.
 	long event_id=request.get("event_id").longValue();
-	
+/*	
 	try{
 		Connection conn = DB.getConnection();
-		PreparedStatement stmt = conn.prepareStatement("SELECT User.first_name, User.last_name from User INNER JOIN Event_has_User User.id=Event_has_User.user_id WHERE Event_has_User.event_id = ? ");
+		PreparedStatement stmt = conn.prepareStatement("SELECT User.first_name, User.last_name from User INNER JOIN Event_has_User on User.id=Event_has_User.user_id WHERE Event_has_Users.event_id=? ");
 		stmt.setLong(1, event_id);
 		ResultSet rs = stmt.executeQuery();
 		JSONArray json_array = new JSONArray();
@@ -675,12 +675,12 @@ public static Result getEventAttendees()
 	}
 	catch(Exception e)
 	{
-
-		return ok(e.toString());
+		e.printStackTrace();
+		return ok("error");
 	}
-	
-	
-	
+	*/
+	return ok("hello world");
+
 }
 
 
