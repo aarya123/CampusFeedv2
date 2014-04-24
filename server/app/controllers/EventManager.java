b1bb29b8cbb26738b39baa39e81f5783bffaac98
mgottein <mgottein@purdue.edu>
2014-04-24 14:31:22 -0400

    added is admin check to update event

--- server/app/controllers/EventManager.java
+++ server/app/controllers/EventManager.java
@@ -524,9 +524,23 @@ public static Result updateEvent()
 		return ok(JsonNodeFactory.instance.objectNode()
 				.put("error", "Parameters: title (string), desc(string), location(string), date_time(long), visibility(int), categories(array)"));
 	}
+	long userId = Application.getUserId(request);
 	Connection conn = DB.getConnection();
 	try {
 		conn.setAutoCommit(false);
+		PreparedStatement isAdminQuery = conn.prepareStatement("select is_admin from CampusFeed.Event_has_User where event_id = ? and user_id = ?");
+		isAdminQuery.setLong(1, id);
+		isAdminQuery.setLong(2, userId);
+		isAdminQuery.execute();
+		boolean isAdmin = false;
+		if(isAdminQuery.getResultSet().next()) {
+			isAdmin = isAdminQuery.getResultSet().getInt(0) != 0;
+		}
+		isAdminQuery.close();
+		if(!isAdmin) {
+			conn.close();
+			return ok(JsonNodeFactory.instance.objectNode().put("no", "not admin"));
+		}
 		PreparedStatement stmt2 = conn.prepareStatement("UPDATE `CampusFeed`.`Event` SET name=?, location=?,description=?,time=?,visibility=?   WHERE `Event`.`id` = ?");
 		stmt2.setString(1, title);
 		stmt2.setString(2, location);
